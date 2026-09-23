a36de5793481d27b4317743291e0b6172fe07b1b
Szabolcs Surányi <sbolch@pm.me>
2026-09-23 20:13:47 +0200

    Add fish config

---
+++ fish/config.fish
@@ -0,0 +1,25 @@
+﻿function fish_greeting
+    fastfetch
+end
+
+if test -f ~/.fish_profile
+  source ~/.fish_profile
+end
+
+alias ls='eza --color=always --group-directories-first --icons=always'
+alias la='eza -a --color=always --group-directories-first --icons=always'
+alias ll='eza -l --color=always --group-directories-first --icons=always'
+alias lt='eza -aT --color=always --group-directories-first --icons=always'
+alias l.="eza -a | grep -e '^\.'" # show only dotfiles
+alias tarnow='tar -acf '
+alias untar='tar -zxvf '
+alias wget='wget -c '
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias hw='hwinfo --short'
